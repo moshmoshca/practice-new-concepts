ad52df4848f7b9ba826566cee2d710e3fc0b9100
Massiel Capellan <massiel.capellan@gmail.com>
2022-02-17 16:19:43 -0400

    Comment out first exercise

--- javaScript-practice/script.js
+++ javaScript-practice/script.js
@@ -1,5 +1,8 @@
-let userName = prompt("What's your name?", 'Write it here');
+// prompt shows a modal windows that allows input
+// Here alert shows string + input from promt
+
+/* let userName = prompt("What's your name?", 'Write it here');
 
 alert(`Your name is ${userName}, nice to meet you!`);
 
-alert(userName);
+alert(userName); */
