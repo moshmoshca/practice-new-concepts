f5f5095de75732f2ae4f8ce277cfb0e16f99e1b5
Massiel Capellan <massiel.capellan@gmail.com>
2022-02-18 08:41:24 -0400

    Add some JS exercises
    I'm still not really sure how some of them work. Commiting to
    finding out.

--- javaScript-practice/script.js
+++ javaScript-practice/script.js
@@ -1,3 +1,5 @@
+/* ---------------- Exercise 01 -------------------- */
+
 // prompt shows a modal windows that allows input
 // Here alert shows string + input from promt
 
@@ -6,3 +8,22 @@
 alert(`Your name is ${userName}, nice to meet you!`);
 
 alert(userName); */
+
+/* ---------------- Exercise 02 -------------------- */
+
+
+/* let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
+
+alert(+a + +b); // 12 */
+
+/* ---------------- Exercise 03 -------------------- */
+
+/* const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
+console.log(percentage); */
+
+/* ---------------- Exercise 04 -------------------- */
+let a = 10;
